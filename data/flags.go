8e5ae1645e6acf72a2afc3f6d65c0f178941e9e9
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-06-16 16:08:22 -0700

    Data flags
    parser types

---
+++ data/flags.go
@@ -0,0 +1,14 @@
+package main
+
+import "flag"
+
+var (
+
+    Parser                  = flag.string("parser", "dimacs", "Parser type. Available Parsers: dimacs")
+    VerticesFilePath        = flag.string("vertices.filePath", "", "Vertices input file path")
+    EdgesFilePath
+
+
+
+
+)
