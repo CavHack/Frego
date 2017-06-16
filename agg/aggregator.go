25357a823b7992612e45062946a0166eebe3ac1b
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-06-15 19:46:02 -0700

    Aggregator
    Interface aggregator for object cloning and memalloc

---
+++ agg/aggregator.go
@@ -0,0 +1,17 @@
+package aggregator
+
+type Aggregator interface {
+
+      Type()  string
+
+
+      Get()   interface{}
+      Set(value interface{}) error
+
+      GetBytes() ([]byte, error)
+      SetBytes(bytes []byte) error
+
+      Clone() Aggregator
+
+
+}
