ec33510c566479ccb57e7c8d7a38dc5a07ffb230
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-07-24 00:28:23 -0700

    Edge Accounting
    Remove, Add, etc… parse as interface{} instances.

--- Algo/handlers.go
+++ Algo/handlers.go
@@ -4,3 +4,52 @@ import (
 
 		"github.com/cavhack/frego"
 )
+
+type Handlers struct {
+
+	OnDuplicateVertex        DuplicateVertexFunc
+	OnMissingVertex			 MissingVertexFunc
+	OnDuplicateEdge			 DuplicateEdgeFunc
+	OnMissingEdge			 MissingEdgeFunc
+
+
+}
+
+type DuplicateVertexFunc func(id string, value1 interface{}, value2 interface{})(finalValue interface{}, err error)
+type MissingVertexFunc func(id string) (initialValue interface{}, err error)
+type DuplicateEdgeFunc func(from string, to string, value1 interface{}, value2 interface{})(finalValue interface{}, err error)
+type MissingEdgeFunc func(from string, to string) (initialValue, interface{}, err error)
+
+func DefaultHandlers() *Handlers {
+
+				return &Handlers{onDuplicateVertex, onMissingVertex, onDuplicateEdge, onMissingEdge}
+
+}
+
+func onDuplicateVertex() {
+
+}
+
+func onMissingVertex(id string) (initialValue interface{}, err error) {
+	glog.Infof("running default onDuplicateEdge handler; edge from %s to %s", from, to)
+	return value1, nil
+}
+
+func onDuplicateEdge() {
+
+}
+
+func onMissingEdge(from string, to string) (initialValue interface{}, err error) {
+
+	glog.Info("running default onMissingEdge handler; edge from %s to %s", from, to)
+	return nil, nil
+}
+
+
+
+
+
+
+
+
+
